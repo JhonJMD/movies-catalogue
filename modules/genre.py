f6ce6029bebdc1301faf9700fe200a507057d9bf
JhonJMD <152086681+JhonJMD@users.noreply.github.com>
2024-04-30 15:19:00 -0500

    feat: :sparkles: Add list Genres
    Add in module genre function list genres

--- modules/genre.py
+++ modules/genre.py
@@ -1,32 +1,55 @@
+# Import necessary modules
+from tabulate import tabulate
 import modules.files as FILE
 import modules.screen as SCREEN
-import modules.reusable as VAL 
+import modules.reusable as VAL
 
+# Check if the file 'genre.json' exists
+FILE.checkFile('genre.json')
+# Read the content of 'genre.json' and store it in the variable 'data'
+data = FILE.readFile('genre.json')
+
+# Function to add a genre
 def addGenre():
     isAddGenre = True
     while isAddGenre:
-        SCREEN.cleanScreen() #Clean console
-        FILE.checkFile('genre.json') #Check that file is create
-        data = FILE.readFile('genre.json') #Save the content of the json into new variable
-        # Dictionary of Genre
+        SCREEN.cleanScreen() # Clear the console screen
+        # Dictionary to store genre information
         genre = {
             "id": "",
             "name": "",
             "description": ""
         } 
-        #Input the information into dictionary
-        while True: #Cycle to check if the ID is repeated or not
+        # Input genre information into the dictionary
+        while True: # Loop to check if the ID is unique
             SCREEN.cleanScreen()
+            # Validate and input the genre's ID
             genre["id"] = VAL.validationNumbers("Genre's ID: ")
+            # Check if the ID is already registered
             if str(genre['id']) in data.keys():
                 SCREEN.cleanScreen()
                 print('This ID is already registered')
                 SCREEN.pauseScreen()
             else:
                 break
-        genre["name"] = str(input("Genre's name: "))
-        genre["description"] = str(input("Description the genre: "))
+        genre["name"] = str(input("Genre's name: ")) # Input genre's name
+        genre["description"] = str(input("Description of the genre: ")) # Input genre's description
         SCREEN.cleanScreen()
+        # Update data with the new genre
         data.update({genre["id"]:genre})
-        FILE.updateFile('genre.json',data)
-        isAddGenre = VAL.validationOtherInput('You want to enter another genre? (Yes(S) -- Enter(No)): ')
+        # Update the 'genre.json' file with the new data
+        FILE.updateFile('genre.json', data)
+        # Ask user if they want to enter another genre
+        isAddGenre = VAL.validationOtherInput('Do you want to enter another genre? (Yes(S) -- Enter(No)): ')
+
+# Function to list all genres
+def listGenre():
+    SCREEN.cleanScreen() # Clear the console screen
+    kys = ['ID','NAME','DESCRIPTION'] # Define column headers
+    rows = [] # Initialize a list to store rows of genre data
+    for key, value in data.items(): # Loop through each genre in the data
+        row = [value["id"], value["name"], value["description"]] # Create a row with genre information
+        rows.append(row) # Add the row to the list of rows
+    # Print the genre data in a formatted table
+    print(tabulate(rows, headers=kys, tablefmt='fancy_grid'))
+    SCREEN.pauseScreen() # Pause the screen for user interaction
